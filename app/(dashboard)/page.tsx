981e4f1ec26afa847807fbbb4e216aeab1ef9f5c
Ruchika <ruchika.3brain@gmail.com>
2024-06-23 00:34:05 +0530

    Worked on the header, navigation ft mobile

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
     <>
-    <UserButton afterSignOutUrl="/"/>
+    <div>Dashboard Page</div>
    </>
   );
 }
---
+++ components/header-logo.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import Image from "next/image"
+
+
+const HeaderLogo = () => {
+  return (
+    <Link href="/">
+            <div className="items-center hidden lg:flex">
+                <Image src="/logo.svg" alt="Logo" height={28} width={28} className="text-white"/>
+                <p className="font-semibold text-white text-2xl ml-2.5">NovaFlux</p>
+            </div>
+    </Link>
+  )
+}
+
+export default HeaderLogo
